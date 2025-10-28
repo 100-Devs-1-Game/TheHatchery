1628a861c2828202dd3d6178b3f67b9568546855
galesy <galesyfailsy@gmail.com>
2025-10-28 15:35:41 -0300

    Debug text fix

--- src/scripts/debug.gd
+++ src/scripts/debug.gd
@@ -7,7 +7,7 @@ func _process(_delta):
 	# NOTE: DISPLAYS CURRENT CLICK VALUE
 	#$Label.text = "%d clicks" % int(clicks)
 	if clicks == 1:
-		$ClickCounter.text = str(clicks) + " click"
+		$"../ClickCounter".text = str(clicks) + " click"
 	else:
-		$ClickCounter.text = str(clicks) + " clicks"
+		$"../ClickCounter".text = str(clicks) + " clicks"
 	#print($Timer.wait_time - $Timer.time_left)
