023d4f246c5c0b17a67fe038df0000257a7aa5d4
CRYPTCRATE <cryptcrate@outlook.com>
2025-11-01 01:55:39 +0000

    Fixed time display format (hh:mm)
    Also separated date to display in new line underneath and placed whole display within main game scene

--- src/scripts/time_display.gd
+++ src/scripts/time_display.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-@onready var display: Label = $Label
+@onready var timedisplay: Label = $Label
+@onready var datedisplay: Label = $Label2
 
 func _input(event: InputEvent) -> void:
 	var hours = Data.clicks % 24
@@ -12,4 +13,5 @@ func _input(event: InputEvent) -> void:
 	var stringed_days = "0" + str(days) if days < 10 else str(days)
 	var stringed_months = "0" + str(months) if months < 10 else str(months)
 	var stringed_years = "0" + str(years) if years < 10 else str(years)
-	display.text = "00:" + stringed_hours + " / " + stringed_days + " / " + stringed_months + " / " + stringed_years
+	timedisplay.text = stringed_hours + ":00"
+	datedisplay.text = stringed_days + " / " + stringed_months + " / " + stringed_years
